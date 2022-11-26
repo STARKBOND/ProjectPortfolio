988dba276474b3e7829ba0367a037e434b949aa7
root <jbraseth@uccs.edu>
2022-11-26 08:00:50 +0000

    Refactored unit tests to account for the upcoming user requirement and integration test for project feature.

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -1,22 +1,58 @@
+# Could be required to authenticate after `user = create(:user)`
+  # allow(controller).to receive(:authenticate_user!).and_return(true)
+  # allow(controller).to receive(:current_user).and_return(@user)
+
 require "rails_helper"
 
 RSpec.describe ProjectsController, type: :controller do
   context "stub" do
     before(:all) do
-      user = create(:user)
-    end
-    context "GET #index" do
-      it "returns a success response" do
-        get :index
-        expect(response).to have_http_status(:success)
+      RSpec::Mocks.with_temporary_scope do
+        user = create(:user)
+        # authenticate?
+        project = Project.create(title: "Test title", description: "Test description")
       end
-    end
 
-    context "GET #show" do
-      let!(:project) { Project.create(title: "Test title", description: "Test description") }
-      it "returns a success response" do
-        get :show, params: { id: project }
-        expect(response).to have_http_status(:success)
+      context "GET #index" do
+        it "returns a success response (any user is able to see projects homepage)" do
+          get :index
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "GET #show" do
+        it "returns a success response (any user is able to show a project)" do
+          get :show, params: { id: project }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "GET projects#new" do
+        it "returns a success response (user is signed in)" do
+          get :new
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "POST projects#create" do
+        it "returns a success response (user is signed in)" do
+          post :create, params: { id: project }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "GET projects#edit" do
+        it "returns a success response (user is signed in)" do
+          get :edit, params: { id: project }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "GET projects#edit" do
+        it "returns a success response (user is signed in)" do
+          delete :destroy, params: { id: project }
+          expect(response).to have_http_status(:success)
+        end
       end
     end
   end
