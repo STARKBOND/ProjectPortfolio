0b8738c8b42238d50b89853c87725cce4fce3e6d
root <jbraseth@uccs.edu>
2022-11-27 00:26:30 +0000

    Tests are Red without DatabaseCleaner. Ready to install. [ci skip]

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -4,56 +4,57 @@
 
 require "rails_helper"
 
+# RSpec.describe ProjectsController, type: :controller do
+#   before(:context) do
+#     RSpec::Mocks.with_temporary_scope do
+#       @user = create(:user)
+#     end
+#   end
+#   it "should print user" do
+#     puts @user.inspect 
+#   end
+# end
+
 RSpec.describe ProjectsController, type: :controller do
-  context "stub" do
-    before(:all) do
-      RSpec::Mocks.with_temporary_scope do
-        user = create(:user)
-        # authenticate?
-        project = Project.create(title: "Test title", description: "Test description")
-      end
+  before(:context) do
+    RSpec::Mocks.with_temporary_scope do
+      @user = create(:user)
+      @project = Project.create(title: "Test title", description: "Test description")
+    end
+  end
+  context "GET #index" do
+    it "returns a success response (any user is able to see projects homepage)" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      context "GET #index" do
-        it "returns a success response (any user is able to see projects homepage)" do
-          get :index
-          expect(response).to have_http_status(:success)
-        end
-      end
+  context "GET #show" do
+    it "returns a success response (any user is able to show a project)" do
+      get :show, params: { :id => @project.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      context "GET #show" do
-        it "returns a success response (any user is able to show a project)" do
-          get :show, params: { id: project }
-          expect(response).to have_http_status(:success)
-        end
-      end
+  context "GET projects#new" do
+    it "returns a success response (user is signed in)" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      context "GET projects#new" do
-        it "returns a success response (user is signed in)" do
-          get :new
-          expect(response).to have_http_status(:success)
-        end
-      end
+  context "POST projects#create" do
+    it "redirects user to projects page after successful creation" do
+      project_attributes = { :title => @project.title, :description => @project.description }
+      post :create, params: { :project => project_attributes }
+      expect(response).to have_http_status('302')
+    end
+  end
 
-      context "POST projects#create" do
-        it "returns a success response (user is signed in)" do
-          post :create, params: { id: project }
-          expect(response).to have_http_status(:success)
-        end
-      end
-
-      context "GET projects#edit" do
-        it "returns a success response (user is signed in)" do
-          get :edit, params: { id: project }
-          expect(response).to have_http_status(:success)
-        end
-      end
-
-      context "GET projects#edit" do
-        it "returns a success response (user is signed in)" do
-          delete :destroy, params: { id: project }
-          expect(response).to have_http_status(:success)
-        end
-      end
+  context "GET projects#edit" do
+    it "redirects user to edit project page" do
+      get :edit, params: { :id => @project.id }
+      expect(response).to have_http_status(:success)
     end
   end
 end
