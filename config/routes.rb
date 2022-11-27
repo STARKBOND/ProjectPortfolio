603e9600cf85cf7df13432c4b29ef5fc955cd60e
root <jbraseth@uccs.edu>
2022-11-27 01:49:42 +0000

    Installed Devise for real. [ci skip]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "projects#index"
   resources :projects
-  # devise_for :users
+  devise_for :users, 
+    controllers: { sessions: 'users/sessions' },   
+    path: 'auth', 
+    path_names: { sign_in: 'login', 
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      unlock: 'unblock', 
+      registration: 'register',
+      sign_up: 'cmon_let_me_in' }
 end
