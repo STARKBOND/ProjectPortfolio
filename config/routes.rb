245dcc89fd9ce63924b896c8b2695e6de6a2ec2e
root <jbraseth@uccs.edu>
2022-11-30 04:47:39 +0000

    Finished renaming everything to Log In/Log Out and Register.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-  root "projects#index"
+  root 'projects#index'
   resources :projects
   devise_for :users, 
-    controllers: { sessions: 'users/sessions' },
+    controllers: { sessions: 'users/sessions', registrations: 'users/registrations' },
     path: 'auth',
     path_names: { 
       sign_in: 'login', 
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
       confirmation: 'verification',
       unlock: 'unblock', 
       registration: 'register',
-      sign_up: 'cmon_let_me_in' }
+      sign_up: 'cmon_let_me_in' 
+    }
 end
