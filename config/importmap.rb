8b2f9a6c0d0db086c77661b87063e3bc6f73e814
root <jbraseth@uccs.edu>
2022-11-30 18:11:22 +0000

    Installed Bootstrap (build fix).

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,5 @@
-pin "bootstrap", to: "bootstrap.min.js", preload: true
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "bootstrap", to: "bootstrap.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
