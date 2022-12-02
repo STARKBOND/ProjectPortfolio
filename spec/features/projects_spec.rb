f9b309e77f5895544a35b123143c557a626adfb0
root <jbraseth@uccs.edu>
2022-12-02 03:55:19 +0000

    Prepare for v1.0.0 release.

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -5,21 +5,23 @@ RSpec.feature "Projects", type: :feature do
   context "User / Project stub" do
     before(:each) do
       user = User.create(:id => 1, :email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+      @email = "user@example.com"
+      @password = "password"
       visit new_user_session_path
       within("form") do
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "password"
+        fill_in "inputEmail", with: @email
+        fill_in "inputPassword", with: @password
       end
-      click_on "Log in"
+      click_on "logIn"
       visit new_project_path
       within("form") do
-        fill_in "Title", with: "Test title"
+        fill_in "title", with: "Test title"
       end
     end
 
     scenario "should be successful" do
-      fill_in "Description", with: "Test description"
-      click_button "Create Project"
+      fill_in "description", with: "Test description"
+      click_button "createProject"
       expect(page).to have_content("Project was successfully created")
       Project.destroy_all
     end
@@ -38,17 +40,17 @@ RSpec.feature "Projects", type: :feature do
 
       scenario "should be successful" do
         within("form") do
-          fill_in "Description", with: "New description content"
+          fill_in "description", with: "New description content"
         end
-        click_button "Update Project"
+        click_button "editProject"
         expect(page).to have_content("Project was successfully updated")
       end
 
       scenario "should fail" do
         within("form") do
-          fill_in "Description", with: ""
+          fill_in "description", with: ""
         end
-        click_button "Update Project"
+        click_button "editProject"
         expect(page).to have_content("Description can't be blank")
       end
     end
@@ -58,23 +60,23 @@ RSpec.feature "Projects", type: :feature do
     setup do
       visit new_user_session_path
       within("form") do
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "password"
+        fill_in "inputEmail", with: "user@example.com"
+        fill_in "inputPassword", with: "password"
       end
-      click_on "Log in"
+      click_on "logIn"
     end
     
     scenario "remove project" do
       Project.destroy_all
       Project.create(id: 1, title: "Test title", description: "Test content")
       visit project_path(Project.find(1))
-      click_on "Destroy this project"
+      click_on "destroyProject"
       expect(page).to have_content("Project was successfully destroyed")
       expect(Project.count).to eq(0)
     end
 
     scenario "Log out" do
-      click_on "Log Out"
+      click_on "logOut"
       expect(page).to have_content("Logged out successfully.")
     end
   end
