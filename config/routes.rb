cdd7ce58049f4cb2d1a5f1c7930dd8abaa0e91fb
root <jbraseth@uccs.edu>
2022-12-11 02:33:18 +0000

    Custom styles improved. Heroku back online.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
   root 'projects#index'
   resources :projects
   devise_for :users, 
@@ -20,15 +19,4 @@ Rails.application.routes.draw do
       registration: 'register',
       sign_up: 'cmon_let_me_in' 
     }
-    # devise_scope :user do
-    #   get 'login', to: 'users/sessions#new'
-    #   post 'login', to: 'users/sessions#create'
-    #   get 'logout', to: 'users/sessions#destroy'
-    #   get 'cmon_let_me_in', to: 'users/registrations#new'
-    #   post '', to: 'users/registrations#create'
-    #   get 'edit', to: 'users/registrations#edit'
-    #   put '', to: 'users/registrations#update'
-    #   get 'cancel', to: 'users/registrations#cancel'
-    #   delete '', to: 'users/registrations#destroy'
-    # end
 end
